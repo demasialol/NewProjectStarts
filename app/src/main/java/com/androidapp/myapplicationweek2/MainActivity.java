217c118a5258d92caa9cb6ecb6f8638151dacf36
demasialol <demasialol@naver.com>
2021-03-11 15:50:33 +0900

    Initial commit

---
+++ app/src/main/java/com/androidapp/myapplicationweek2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.androidapp.myapplicationweek2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//home view
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
